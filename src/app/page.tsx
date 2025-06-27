f3fbefddf9b24ddd309d0da5916faae265239008
Vitor Barreira <vitor@MMBBOX000002SP.local>
2025-06-27 16:41:44 -0300

    fix: Corrige variável não utilizada no build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, _session) => {
       // Se o evento for SIGNED_IN, o usuário acabou de logar.
       if (event === 'SIGNED_IN') {
         router.push('/dashboard')
